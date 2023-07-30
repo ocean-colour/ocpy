8407e489904f90b4bbf2e88789243dc20d70625e
profxj <xavier@ucolick.org>
2023-07-30 06:26:14 -0700

    rebin

--- oceancolor/tara/spectra.py
+++ oceancolor/tara/spectra.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import pandas
+from scipy.interpolate import interp1d
 
 from IPython import embed
 
@@ -188,3 +189,65 @@ def single_value(tbl:pandas.DataFrame, wv_cen:float,
     
     # Return
     return value, sig
+
+def interpolate_to_grid(wv_nm:np.ndarray, values:np.ndarray, 
+                  err_vals:np.ndarray, wv_grid:np.ndarray):
+    """ Rebin a spectrum to a new wavelength grid.
+
+    Args:
+        wv_nm (np.ndarray): Wavelengths (nm)
+        values (np.ndarray): Values
+        err_vals (np.ndarray): Error values
+        wv_grid (np.ndarray): New wavelength grid
+
+    Returns:
+        tuple: values, error [np.ndarray, np.ndarray]
+    """
+    # Interpolate
+    f_values = interp1d(wv_nm, values, 
+        bounds_error=False, fill_value=np.nan)
+    f_err = interp1d(wv_nm, err_vals, 
+        bounds_error=False, fill_value=np.nan)
+
+    # Evaluate
+    new_values = f_values(wv_grid)
+    new_err = f_err(wv_grid)
+
+    # Return
+    return new_values, new_err
+
+def rebin_to_grid(wv_nm:np.ndarray, values:np.ndarray, 
+                  err_vals:np.ndarray, wv_grid:np.ndarray):
+    """ Rebin spectra to a new wavelength grid.
+
+    Args:
+        wv_nm (np.ndarray): Wavelengths (nm)
+        values (np.ndarray): Values
+        err_vals (np.ndarray): Error values
+        wv_grid (np.ndarray): New wavelength grid
+
+    Returns:
+        tuple: values, error [np.ndarray, np.ndarray]
+    """
+    gd_values = np.isfinite(values)
+    mask = gd_values.astype(int)
+
+    # Loop on wv_grid
+    rebin_values = np.zeros((wv_grid.size-1, values.shape[1]))
+    rebin_wave = np.zeros(wv_grid.size-1)
+    
+    for iwv in range(wv_grid.size-1):
+        w0 = wv_grid[iwv]
+        w1 = wv_grid[iwv+1]
+        rebin_wave[iwv] = (w0+w1)/2.
+        # In grid?
+        gd = np.where((wv_nm >= w0) & (wv_nm < w1))[0]
+
+        # Add em in
+        isum = np.nansum(values[gd]*mask[gd], axis=0) / np.sum(mask[gd],axis=0)
+
+        # Fill
+        rebin_values[iwv] = isum
+
+    # Return
+    return rebin_wave, rebin_values

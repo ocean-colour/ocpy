02d4d11ad5dc6f68070f7681cb61ae0491ff5655
J. Xavier Prochaska <xavier@ucolick.org>
2023-10-22 19:03:17 -0700

    bspline

--- proposals/PACE2023/py/figs_pace2023.py
+++ proposals/PACE2023/py/figs_pace2023.py
@@ -18,6 +18,9 @@ from matplotlib.ticker import MultipleLocator
 from oceancolor.ihop import pca as ihop_pca
 from oceancolor.utils import plotting 
 
+from pypeit.core import fitting
+from pypeit.bspline import bspline
+
 mpl.rcParams['font.family'] = 'stixgeneral'
 
 
@@ -100,6 +103,74 @@ def fig_l23_tara_pca(outfile='fig_l23_tara_pca.png'):
     print(f"Saved: {outfile}")
 
 
+def fig_bspline_tara(outfile='fig_bspline_tara.png'):
+
+    # Load up
+    wv_grid, tara_a_water, l23_a = ihop_pca.load_tara()
+    wv_grid = wv_grid.astype(np.float64)
+
+    L23_Tara_pca = ihop_pca.load_pca(f'pca_L23_X4Y0_Tara_a_N3.npz')
+
+    imx = np.argmax(tara_a_water[:,3])
+
+
+    fig = plt.figure(figsize=(12,6))
+    plt.clf()
+    gs = gridspec.GridSpec(1,2)
+
+    # #####################################################
+    # RMS
+    ax_rms= plt.subplot(gs[0])
+
+    # #####################################################
+    # Spectra
+    ax_spec= plt.subplot(gs[1])
+
+    # B-spline it
+    ifit = 1
+    everyns = [2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+    for clr, idx in zip(['b', 'g'], [100000, imx]):
+        orig, recon = ihop_pca.reconstruct(L23_Tara_pca, idx)
+        orig = orig.astype(np.float64)
+        sv_fits = [] 
+        rmss = []
+        for everyn in everyns:
+            my_bspline = bspline(wv_grid, nord=3,
+                        everyn=everyn)
+            code, yfit = my_bspline.fit(wv_grid, orig, np.ones_like(orig))
+            sv_fits += [yfit]
+            # RMS
+            rmss += [np.sqrt(np.mean((orig - yfit)**2))]
+
+        # Plot RMS vs. everyn
+        ax_rms.plot(everyns, rmss, 'o', color=clr)
+
+        ax_rms.set_xlabel('Every n')
+        ax_rms.set_ylabel(r'RMS')
+        #ax_rms.set_ylim(-1., 1.1*np.max(orig))
+
+
+        ax_spec.plot(wv_grid, orig,  'o', color=clr, label=f'Tara: idx={idx}')
+        # Fit
+        ax_spec.plot(wv_grid, sv_fits[ifit],  ls='-', color=clr,
+                     label=f'B-Spline: everyn={everyns[ifit]}')
+
+        ax_spec.set_xlabel('Wavelength (nm)')
+        ax_spec.set_ylabel(r'$a(\lambda)$')
+        ax_spec.set_ylim(0., 1.1*np.max(orig))
+        ax_spec.legend(fontsize=15.)
+        
+
+    # Finish
+    for ax in [ax_rms, ax_spec]:
+        plotting.set_fontsize(ax, 15)
+
+    plt.tight_layout()#pad=0.0, h_pad=0.0, w_pad=0.3)
+    plt.savefig(outfile, dpi=300)
+    print(f"Saved: {outfile}")
+
+
 def main(flg):
     if flg== 'all':
         flg= np.sum(np.array([2 ** ii for ii in range(25)]))
@@ -110,6 +181,10 @@ def main(flg):
     # PCA
     if flg & (2**0):
         fig_l23_tara_pca()
+
+    # b-splines
+    if flg & (2**1):
+        fig_bspline_tara()
 
 
 # Command line execution

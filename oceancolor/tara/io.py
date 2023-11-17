f42d3dbf8cd6067755f50cc80eb5ca26d27694da
profxj <xavier@ucolick.org>
2023-11-16 16:39:22 -0800

    wip

--- oceancolor/tara/io.py
+++ oceancolor/tara/io.py
@@ -20,3 +20,11 @@ def load_tara_db():
     df = pandas.read_parquet(db_name)
     # Return
     return df
+
+def load_pg_db():
+    pg_db_name = os.path.join(resource_filename(
+        'oceancolor', 'data'), 'Tara', 'merged_tara_pacific_microbiome_acs.feather')
+    df = pandas.read_feather(pg_db_name)
+
+    # Return
+    return df

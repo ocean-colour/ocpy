b26b24f17327203eb2bd5ae98fca470630fce2c9
profxj <xavier@ucolick.org>
2023-12-07 05:42:24 -0800

    wip

--- oceancolor/hydrolight/loisel23.py
+++ oceancolor/hydrolight/loisel23.py
@@ -5,9 +5,6 @@ import xarray
 import numpy as np
 
 from oceancolor.tara import io as tara_io 
-from oceancolor.tara import spectra
-from oceancolor import water
-from oceancolor.utils import spectra as spec_utils 
 
 l23_path = os.path.join(os.getenv('OS_COLOR'),
                         'data', 'Loisel2023')
@@ -38,63 +35,3 @@ def load_ds(X:int, Y:int):
 
     # Return
     return ds
-
-def tara_matched_to_l23(low_cut:float=400., high_cut:float=705., X:int=4, Y:int=0):
-    """ Generate Tara spectra matched to L23
-
-    Restricted on wavelength and time of cruise
-    as per Patrick Gray recommendations
-
-    Args:
-        low_cut (float, optional): low cut wavelength. Defaults to 400..
-        high_cut (float, optional): high cut wavelength. Defaults to 705..
-        X (int, optional): simulation scenario. Defaults to 4.
-        Y (int, optional): solar zenith angle used in the simulation.
-            Defaults to 0.
-
-    Returns:
-        tuple: wavelength grid, Tara spectra, L23 spectra
-    """
-
-    # Load up the data
-    l23_ds = load_ds(X, Y)
-
-    # Load up Tara
-    print("Loading Tara..")
-    tara_db = tara_io.load_tara_db(clean_dates=True)
-
-    # Spectra
-    wv_nm, all_a_p, all_a_p_sig = spectra.spectra_from_table(tara_db)
-
-    # Wavelengths, restricted to > 400 nm
-    cut = (l23_ds.Lambda > low_cut) & (l23_ds.Lambda < high_cut)
-    l23_a = l23_ds.a.data[:,cut]
-
-    # Rebin
-    wv_grid = l23_ds.Lambda.data[cut]
-    tara_wv = np.append(wv_grid, [high_cut+5.]) - 2.5 # Because the rebinning is not interpolation
-    rwv_nm, r_ap, r_sig = spectra.rebin_to_grid(
-        wv_nm, all_a_p, all_a_p_sig, tara_wv)
-
-    # Add in water
-    print("Adding in water..")
-    df_water = water.load_rsr_gsfc()
-    a_w, _ =  spec_utils.rebin(df_water.wavelength.values, 
-                        df_water.aw.values, np.zeros_like(df_water.aw),
-                        wv_grid)
-
-    tara_a_water = r_ap+np.outer(np.ones(r_ap.shape[0]), a_w)
-
-    # Polish Tara for PCA
-    bad = np.isnan(tara_a_water) | (tara_a_water <= 0.)
-    ibad = np.where(bad)
-
-    mask = np.ones(tara_a_water.shape[0], dtype=bool)
-    mask[np.unique(ibad[0])] = False
-
-    # Cut down: Aggressive but necessary
-    tara_a_water = tara_a_water[mask,:]
-
-
-    # Return
-    return wv_grid, tara_a_water, l23_a

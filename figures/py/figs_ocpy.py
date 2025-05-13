56bcd5c4768535387350a54462e25694371b6b49
profxj <xavier@ucolick.org>
2025-05-13 08:01:52 -0700

    better

--- figures/py/figs_ocpy.py
+++ figures/py/figs_ocpy.py
@@ -5,6 +5,8 @@ import numpy as np
 
 from matplotlib.patches import Ellipse
 
+from ocpy.utils import fig_utils
+
 
 def fig_stommel():
     # Create a figure and axis
@@ -21,11 +23,11 @@ def fig_stommel():
 
     # Customize x-axis tick labels
     tick_labels = ['1 mm', '1 cm', '1 dm', '1 m', '10 m', '100 m', '1 km', '10 km',
-                   '100 km', '1000 km', '10,000 km']  # Corresponding labels
+                   r'$10^2$ km', r'$10^3$ km', r'$10^4$ km']  # Corresponding labels
     ticks = np.arange(0, 11)  # Ticks from 0 to 10
     ax.set_xticks(ticks)
     ax.set_xticklabels(tick_labels)
-    ax.set_xlim(0, 10)  # Set x-axis limits
+    ax.set_xlim(-0.5, 11.0)  # Set x-axis limits
 
     # Y-axis ticks are in time scale
     y_ticks = np.arange(9)
@@ -33,11 +35,8 @@ def fig_stommel():
                     '1 year', '10 years', '100 years']
     ax.set_yticks(y_ticks)
     ax.set_yticklabels(ytick_labels)
-    ax.set_ylim(0, 8)  # Set y-axis limits
+    ax.set_ylim(-0.5, 8)  # Set y-axis limits
 
-    # Set axis labels and title
-    ax.set_xlabel("Spatial Scale")
-    ax.set_ylabel("Time Scale (log)")
     #ax.set_title("Logarithmic Spatial and Time Scales")
 
     # Add a legend
@@ -45,8 +44,56 @@ def fig_stommel():
 
     # Add ellipses for the physical processes
     ellipse_data = [
+        # Physical processes
         {"center": (0.5, 0.7), "width": 2, "height": 1.5, "angle": 0, "color": 
-            "lightblue", "text": "Molecular\nProcesses", "label": "Molecular"},
+            "lightblue", "text": "Molecular\nProcesses", "label": "Molecular",
+            "label_off": (0.0, 0.0)},
+        {"center": (2.7, 1.5), "width": 2, "height": 1.5, "angle": 0, "color": 
+            "lightblue", "text": "Tubulent\nPatches", "label": "Molecular",
+            "label_off": (0.0, -0.2)},
+        {"center": (4.1, 1.7), "width": 2, "height": 1.5, "angle": 0, "color": 
+            "lightblue", "text": "Langmuir\nCells", "label": "Molecular",
+            "label_off": (0.0, 0.0)},
+        {"center": (4.0, 1.3), "width": 3, "height": 1.5, "angle": 0, "color": 
+            "lightblue", "text": "Surface\nWaves", "label": "Molecular",
+            "label_off": (0.0, -0.5)},
+        {"center": (5.1, 2.0), "width": 3, "height": 2.0, "angle": 0, "color": 
+            "lightblue", "text": "Inertial\nWaves", "label": "Molecular",
+            "label_off": (0.2, +0.1)},
+        {"text": "Internal\nTides", "center": (7.1, 2.8), "width": 1, "height": 0.5, "angle": 0, 
+            "color": "lightblue", "label": "Molecular", "label_off": (0.0, -0.1)},
+        {"text": "Fronts\nEddies", "center": (7.2, 5.0), 
+         "width": 2.1, "height": 1.7, "angle": 0, "color": "lightblue", "label": "Molecular", "label_off": (0.0, +0.0)},
+        {"text": "Coastal\nWaves", "center": (7.3, 4.1), 
+         "width": 0.5, "height": 1.0, "angle": 0, "color": "lightblue", "label": "Molecular", "label_off": (0.0, +0.0)},
+        {"text": "Surface\nTides", "center": (8.5, 2.7), 
+         "width": 1.0, "height": 0.5, "angle": 0, "color": "lightblue", "label": "Molecular", "label_off": (0.0, +0.0)},
+        {"text": "Mixed Layer\nDepth", "center": (8.5, 5.5), 
+         "width": 1.0, "height": 1.0, "angle": 0, "color": "lightblue", "label": "Molecular", "label_off": (0.0, +0.0)},
+        {"text": "Rossby\nWaves", "center": (9.5, 6.0), 
+         "width": 1.7, "height": 1.0, "angle": 0, "color": "lightblue", "label": "Molecular", "label_off": (0.0, +0.0)},
+        {"text": "ENSO", "center": (10.2, 6.5), 
+         "width": 1.0, "height": 0.5, "angle": 0, "color": "lightblue", "label": "Molecular", "label_off": (0.0, +0.0)},
+        # Biological processes
+        {"center": (1.2, 1.5), "width": 2.5, "height": 1.5, "angle": 0, "color": 
+            "pink", "text": "Individual\nMovement", "label": "Molecular",
+            "label_off": (0.0, 0.0)},
+        {"center": (5.0, 3.2), "width": 2.5, "height": 1.5, "angle": 0, "color": 
+            "pink", "text": "Plankton\nMigration", "label": "Molecular",
+            "label_off": (0.0, 0.0)},
+        {"center": (5.5, 4.5), "width": 3.2, "height": 1.5, "angle": 0, "color": 
+            "pink", "text": "Phytoplankton\nBlooms", "label": "Molecular",
+            "label_off": (0.0, 0.0)},
+        {"text": "Biomass\nCycles", "center": (8.0, 5.5), 
+         "width": 1.3, "height": 1.0, "angle": 0, "color": "pink", 
+         "label": "Molecular", "label_off": (+0.4, -0.5)},
+        # Climate
+        {"center": (10.2, 8.1), "width": 1.0, "height": 2.0, "angle": 0, "color": 
+            "lightgreen", "text": "Climate", "label": "Molecular",
+            "label_off": (0.0, -0.5)},
+        {"center": (10.2, 7.0), "width": 1.0, "height": 0.3, "angle": 0, "color": 
+            "lightgreen", "text": "Decadal\nOscillations", "label": "Molecular",
+            "label_off": (0.0, 0.0)},
     ]
 
     # Add all ellipses and their text to the plot
@@ -58,14 +105,65 @@ def fig_stommel():
         ax.add_patch(ellipse)
         
         # Add text to the center of each ellipse
-        ax.text(data["center"][0], data["center"][1], data["text"],
+        fclr = 'black' if data["color"] in ['lightblue','lightgreen'] else 'gray'
+        ax.text(data["center"][0]+data['label_off'][0],
+                data["center"][1]+data['label_off'][1], 
+                data["text"], 
                 horizontalalignment='center',
                 verticalalignment='center',
                 fontsize=8,
                 fontweight='bold',
-                color='black')
+                color=fclr)
+
+    # Add an arrow with two heads
+    aclr = 'r'
+    ax.annotate('', xy=(0.5, 0.5), xytext=(9.5, 7.5),
+                arrowprops=dict(arrowstyle='<->', lw=2, color=aclr),
+                fontsize=8)
+    # Add text to the arrow, rotated
+    ax.text(3.0, 2.8, "Cascade of energy", fontsize=10,
+            horizontalalignment='center', verticalalignment='center',
+            rotation=40, color=aclr, fontweight='bold')
+
+    # Add an empty rectangle for Satellites
+    sat_clr = 'mediumorchid'
+    x_sat = (5.7, 10.8)
+    y_sat = (3.0, 7.3)
+    xy_sat = (x_sat[0], y_sat[0])
+    dx_sat = np.abs(x_sat[1] - x_sat[0])
+    dy_sat = np.abs(y_sat[1] - y_sat[0])
+    rect = plt.Rectangle(xy_sat, dx_sat, dy_sat, angle=0.0,
+                         facecolor='none', edgecolor=sat_clr, lw=2)
+    ax.add_patch(rect)
+    ax.text(8.5, 3.2, "Satellites", fontsize=10,
+            horizontalalignment='left', verticalalignment='center',
+            color=sat_clr, fontweight='bold')
+    
+    # Calypso Monterey Bay
+    cmb_clr = 'gold'
+    x_cmb = (4.0, 8.0)
+    y_cmb = (1.8, 6.5)
+    xy_cmb = (x_cmb[0], y_cmb[0])
+    dx_cmb = np.abs(x_cmb[1] - x_cmb[0])
+    dy_cmb = np.abs(y_cmb[1] - y_cmb[0])
+    rect = plt.Rectangle(xy_cmb, dx_cmb, dy_cmb, angle=0.0,
+                         facecolor='none', edgecolor=cmb_clr, lw=2)
+    ax.add_patch(rect)
+    ax.text(2.0, 6.2, "Calypso\nMonterey\nBay", fontsize=12,
+            horizontalalignment='left', verticalalignment='center',
+            color=cmb_clr, fontweight='bold')
+
+    # Axes fonts
+    fig_utils.set_fontsize(ax, 8)
+    #fig_utils.set_fontweight(ax, 'bold')
+
+    # Set axis labels and title
+    fsz = 12
+    ax.set_xlabel("Horizontal Spatial Scales", fontsize=fsz)
+    ax.set_ylabel("Time Scales", fontsize=fsz)
 
     # Save the figure
+    plt.tight_layout()
     plt.savefig("fig_stommel.png", dpi=300, bbox_inches='tight')
     print("Figure saved as fig_stommel.png")
 

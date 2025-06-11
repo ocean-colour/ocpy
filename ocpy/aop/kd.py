1529840d67949ab392a1769cd0ee4e06109b5da7
profxj <xavier@ucolick.org>
2025-06-11 06:44:57 -0700

    Kd fix

--- ocpy/aop/kd.py
+++ ocpy/aop/kd.py
@@ -3,11 +3,12 @@
 import numpy as np
 
 
-def calc_kd_lee(a:np.ndarray, bb:np.ndarray, bbw:np.ndarray, theta_sun:float=0.):
+def calc_kd_lee(a:np.ndarray, bb:np.ndarray, bbw:np.ndarray, 
+                theta_sun:float=0.):
 
     # Calculate the diffuse attenuation coefficient Kd using the Lee et al. (1998) method.
     Kd = (1. + 1.005*theta_sun) * a + 4.259*(1-0.265*bbw/bb) * (
-        1-0.52*np.exp(-10.8*a))
+        1-0.52*np.exp(-10.8*a))*bb
 
     # Ensure Kd is non-negative
     Kd = np.maximum(Kd, 0)

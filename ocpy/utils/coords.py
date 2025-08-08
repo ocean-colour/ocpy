9557c5c04157339e58f3e762595b981e7c9f587a
profxj <xavier@ucolick.org>
2025-08-08 12:24:00 -0700

    distances

--- ocpy/utils/coords.py
+++ ocpy/utils/coords.py
@@ -1,5 +1,10 @@
 """ Utilities related to coordinates. """
 
+import numpy as np
+from math import radians
+
+from sklearn.metrics.pairwise import haversine_distances
+
 import re
 
 def dms_to_decimal(degrees, minutes, seconds, direction):
@@ -65,3 +70,30 @@ def parse_dms_string(dms_str):
         seconds = 0
     
     return dms_to_decimal(degrees, minutes, seconds, direction)
+
+
+def distance_from_latlon(point:tuple, lat_lons:np.ndarray):
+    """
+    Calculate the distance in km between a point and an array of lat/lon coordinates.
+    
+    Parameters:
+    point (tuple): A tuple containing the latitude and longitude of the point (lat, lon).
+        degrees
+    lat_lons (np.ndarray): A 2D array of shape (n, 2) where each row is a pair of latitude and longitude coordinates.
+        degrees
+    The first column should be latitude and the second column should be longitude.
+    
+    Returns:
+    np.ndarray: An array of distances in kilometers from the point to each coordinate in lat_lons.
+    """
+
+    # Convert to radians
+    origin = np.array([[np.radians(point[0]), np.radians(point[1])]])
+    destinations = np.radians(lat_lons)
+    # Calculate distances (returns in radians)
+    distances_rad = haversine_distances(origin, destinations)
+    # Convert to kilometers (Earth radius = 6371 km)
+    distances_km = distances_rad * 6371
+
+    # Return
+    return distances_km[0]
